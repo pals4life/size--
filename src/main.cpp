ce29eb3c4712eed4054268da5d1bf46981247477
Ward <ward.gauderis@student.uantwerpen.be>
2019-11-21 14:38:59 +0100

    mian

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "pal.h"
 
 int main(int argc, char** argv) {
-	pal::encode("test/shreks.bmp", "test/ward_encoded.pal", Algorithm::bisection);
-//	pal::decode("test/ward_encoded.pal", "test/ward_output");
+	pal::encode("test/shrek.bmp", "test/ward_encoded.pal", Algorithm::bisection);
+	pal::decode("test/ward_encoded.pal", "test/ward_output");
 }
