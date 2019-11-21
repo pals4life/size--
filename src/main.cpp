14f5025afad3009405e899fca1a1e015526f52b8
Ward <ward.gauderis@student.uantwerpen.be>
2019-11-21 14:36:24 +0100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "pal.h"
 
-
 int main(int argc, char** argv) {
-	pal::encode("test/photo.bmp", "test/ward_encoded.pal", Algorithm::bisection);
-	pal::decode("test/ward_encoded.pal", "test/ward_output");
-	
-	system("colordiff test/photo.bmp test/ward_output");
+
+	pal::encode("test/shreks.bmp", "test/ward_encoded.pal", Algorithm::bisection);
+//	pal::decode("test/ward_encoded.pal", "test/ward_output");
+
+//	system("colordiff test/photo.bmp test/ward_output");
 }
