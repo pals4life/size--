ea0be23f98381ed9399615fdc0ef812d0c94cf1a
sharogue <mano.marichal@student.uantwerpen.be>
2019-11-21 13:36:38 +0100

    generic commit message

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include "pal.h"
-#include <timer>
+
 
 int main(int argc, char** argv) {
-	{
-		TimeFunction;
-		std::vector<std::string> arguments(argv + 1, argv + argc);
 
-		pal::encode("test/ward_input", "test/ward_encoded.pal", Algorithm::bisection);
-	}
-	pal::decode("test/ward_encoded.pal", "test/ward_output");
-	system("diff test/ward_input test/ward_output");
 
-//	pal::encode("test/128mb.tar", "test/128mb.tar.pal", Algorithm::bisection);
-//    pal::decode("tst/128mb.tar.pal", "test/new128mb.tar");
+	pal::encode("./test.txt", "../test/test.pal", Algorithm::sequitur);
+	pal::decode("./test/test.pal", "../test/test.txt");
+// pal::decode("tst/128mb.tar.pal", "test/new128mb.tar");
 }
