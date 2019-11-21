73c347ca5bf063bfadb509b699632a8c5d439612
funkymaniac <thomas@dooms.eu>
2019-11-21 11:58:05 +0100

    ward code fix

--- src/util/production.h
+++ src/util/production.h
@@ -13,3 +13,12 @@
 #include <array>
 
 using Production = std::array<uint32_t, 2>;
+
+template<>
+struct std::hash<Production>
+{
+    std::size_t operator()(const Production& p) const noexcept
+    {
+        return (static_cast<uint64_t>(p[0]) << 32u) + p[1];
+    }
+};
