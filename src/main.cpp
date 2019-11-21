65e3113f74615726f11e0ac08f02aef34a473c3b
sharogue <mano.marichal@student.uantwerpen.be>
2019-11-21 14:44:38 +0100

    generic commit message

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
 
 
-    pal::encode("../test/shrek.bmp", "../test/shrek.pal", Algorithm::sequitur);
+    pal::encode("../test/chessboard.bmp", "../test/chessboard.pal", Algorithm::sequitur);
     //pal::encode("../test/beemovie.txt", "../test/beemovie.pal", Algorithm::sequitur);
 	//pal::decode("./test/test.pal", "../test/test.txt");
     //pal::decode("tst/128mb.tar.pal", "test/new128mb.tar");
