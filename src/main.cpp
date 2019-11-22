d8468c7f95117e97ac30cf9275ff19c77a35324e
Ward <ward.gauderis@student.uantwerpen.be>
2019-11-22 22:43:06 +0100

    bisection

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,15 @@ int main(int argc, char** argv)
     pal::encode("input/chessboard.bmp", "compressed/chessboard.pal", Algorithm::bisection);
     pal::decode("compressed/chessboard.pal", "output/chessboard.bmp");
 
-    pal::encode("input/shrek.bmp", "compressed/shrek.pal", Algorithm::none);
+    pal::encode("input/shrek.bmp", "compressed/shrek.pal", Algorithm::bisection);
     pal::decode("compressed/shrek.pal", "output/shrek.bmp");
 
-    pal::encode("input/blue.bmp", "compressed/blue.pal", Algorithm::none);
+    pal::encode("input/blue.bmp", "compressed/blue.pal", Algorithm::bisection);
     pal::decode("compressed/blue.pal", "output/blue.bmp");
 
-    pal::encode("input/sphere.bmp", "compressed/sphere.pal", Algorithm::none);
+    pal::encode("input/sphere.bmp", "compressed/sphere.pal", Algorithm::optimisedBisection);
     pal::decode("compressed/sphere.pal", "output/sphere.bmp");
 
-    pal::encode("input/text.txt", "compressed/text.pal", Algorithm::none);
+    pal::encode("input/text.txt", "compressed/text.pal", Algorithm::bisection);
     pal::decode("compressed/text.pal", "output/text.txt");
 }
