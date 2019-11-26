60c3562323654a0b1f99d8a445ae2ed135e3fc76
sharogue <mano.marichal@student.uantwerpen.be>
2019-11-26 14:41:07 +0100

    main frix

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include "controller.h"
 #include <iostream>
-#include "./pal.h"
 int main(int argc, char** argv)
 {
-    pal::encode("../input/text.txt", "../output/text.txt", Algorithm::lzw);
-	Controller controller(argc, argv);
+    Controller controller(argc, argv);
 	std::cout << controller.getReturnValue() << std::endl;
 	return controller.getReturnValue();
 }
